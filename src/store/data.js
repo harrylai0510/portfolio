19ac3a92094b7bf6fb410db8d07e5e0c04e84b25
Harry Lai <harrylai0510@gmail.com>
2024-04-22 16:23:40 +0800

    Add store data

---
+++ src/store/data.js
@@ -0,0 +1,23 @@
+export const data = {
+    contact: {
+        email: "",
+        phone: ""
+    },
+    education: [
+        {
+            name: "",
+            year: "",
+            school: "",
+        }
+    ],
+    skills: [
+    ],
+    experiences: [
+        {
+            jobTitle: "",
+            company: "",
+            time: "",
+            description: []
+        },
+    ]
+};
