78dbffe327c42e3a1078868a206c62dc52a23775
Harry Lai <harrylai0510@gmail.com>
2024-10-08 19:45:54 +0800

    add todolist application demo

--- src/pages/Application/index.js
+++ src/pages/Application/index.js
@@ -4,10 +4,6 @@ import {Button, Card} from "react-bootstrap";
 import {Link, redirect} from "react-router-dom";
 import './index.css';
 
-function handleButtonClick() {
-    window.location.href = 'https://book.harrylai.com';
-}
-
 export default function Application() {
     return (
         <>
@@ -16,17 +12,23 @@ export default function Application() {
             </div>
 
             <div className={"apps"}>
-                <Row className={"g-2"}>
-                    <Col lg={4}>
+                <Row className={"g-5"}>
+                    <Col lg={6} className="d-flex justify-content-between text-center">
                         <Link to={'https://book.harrylai.com'} style={{ textDecoration: 'none' }}>
                             <Card style={{width: '18rem', boxShadow: '1px 1px 1px'}}>
                                 <Card.Img variant="top" src="images/book.harrylai.com.png"/>
                                 <Card.Body>
                                     <Card.Title>Book System</Card.Title>
-                                    {/*<Card.Text>*/}
-                                    {/*    Borrow Book*/}
-                                    {/*</Card.Text>*/}
-                                    {/*<Button variant="primary" onClick={handleButtonClick}>Demo</Button>*/}
+                                </Card.Body>
+                            </Card>
+                        </Link>
+                    </Col>
+                    <Col lg={6}>
+                        <Link to={'https://todolist.harrylai.com'} style={{ textDecoration: 'none' }}>
+                            <Card style={{width: '18rem', boxShadow: '1px 1px 1px'}}>
+                                <Card.Img variant="top" src="images/todolist.harrylai.com.png"/>
+                                <Card.Body>
+                                    <Card.Title>To-Do List</Card.Title>
                                 </Card.Body>
                             </Card>
                         </Link>
